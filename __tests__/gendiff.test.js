187ef7b1e9a2ad7f3f40038d39a3395483f205db
saintsloth <dakaprielov@gmail.com>
2022-01-14 20:03:23 +0300

    -> for github-actions - 5

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,10 +2,17 @@
 
 import * as fs from 'fs';
 import gendiff from '../src/func';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import * as path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('gendiff-test', () => {
-  const filepath1 = '/home/duck/WebstormProjects/frontend-project-lvl2/__fixtures__/file1.json';
-  const filepath2 = '/home/duck/WebstormProjects/frontend-project-lvl2/__fixtures__/file2.json';
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json')
   const result = gendiff(filepath1, filepath2);
   const expectedResult = fs.readFileSync('/home/duck/WebstormProjects/frontend-project-lvl2/__fixtures__/expectedResult', 'utf8');
 
