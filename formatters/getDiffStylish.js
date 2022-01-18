c115b3531139d5489c336c95e4a7dca651c2c039
saintsloth <dakaprielov@gmail.com>
2022-01-18 15:15:29 +0300

    test json

--- formatters/getDiffStylish.js
+++ formatters/getDiffStylish.js
@@ -1,37 +1,8 @@
 import _ from 'lodash';
-
-const getDiff = (obj1, obj2) => {
-  const keys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
-
-  const loop = ([key, ...tail], acc = {}) => {
-    if (key === undefined) return acc;
-    const findKey1 = _.find(_.entries(obj1), ([key1]) => key === key1);
-    const findKey2 = _.find(_.entries(obj2), ([key2]) => key === key2);
-    const [, value1] = findKey1 ?? '';
-    const [, value2] = findKey2 ?? '';
-    if (_.isObject(value1) && _.isObject(value2)) {
-      return loop(tail, _.set(acc, key, getDiff(value1, value2)));
-    }
-    if (value1 === value2) {
-      return loop(tail, _.set(acc, key, value1));
-    }
-    if (findKey1 && findKey2) {
-      return loop(tail, _.set(_.set(acc, `- ${key}`, value1), `+ ${key}`, value2));
-    }
-    if (findKey1) {
-      return loop(tail, _.set(acc, `- ${key}`, value1));
-    }
-    if (findKey2) {
-      return loop(tail, _.set(acc, `+ ${key}`, value2));
-    }
-    return 'never';
-  };
-
-  return loop(keys);
-};
+import getDiffTree from './getDiffTree.js';
 
 const getDiffStylish = (obj1, obj2) => {
-  const result1 = getDiff(obj1, obj2);
+  const result1 = getDiffTree(obj1, obj2);
   const result2 = JSON.stringify(result1, null, 4);
   const result3 = result2.replace(/ ""/g, '');
   const result4 = result3.replace(/,\s/g, '\n');
--- formatters/getDiffTree.js
+++ formatters/getDiffTree.js
@@ -2,24 +2,32 @@ import _ from 'lodash';
 
 const getDiffTree = (obj1, obj2) => {
   const keys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
-  const result = {};
-  keys.forEach((key) => {
+
+  const loop = ([key, ...tail], acc = {}) => {
+    if (key === undefined) return acc;
     const findKey1 = _.find(_.entries(obj1), ([key1]) => key === key1);
     const findKey2 = _.find(_.entries(obj2), ([key2]) => key === key2);
     const [, value1] = findKey1 ?? '';
     const [, value2] = findKey2 ?? '';
     if (_.isObject(value1) && _.isObject(value2)) {
-      result[`  ${key}`] = getDiffTree(value1, value2);
-    } else {
-      if (value1 === value2) {
-        result[`  ${key}`] = value1;
-      } else {
-        if (findKey1) result[`- ${key}`] = value1;
-        if (findKey2) result[`+ ${key}`] = value2;
-      }
+      return loop(tail, _.set(acc, key, getDiffTree(value1, value2)));
     }
-  });
-  return result;
+    if (value1 === value2) {
+      return loop(tail, _.set(acc, key, value1));
+    }
+    if (findKey1 && findKey2) {
+      return loop(tail, _.set(_.set(acc, `- ${key}`, value1), `+ ${key}`, value2));
+    }
+    if (findKey1) {
+      return loop(tail, _.set(acc, `- ${key}`, value1));
+    }
+    if (findKey2) {
+      return loop(tail, _.set(acc, `+ ${key}`, value2));
+    }
+    return 'never';
+  };
+
+  return loop(keys);
 };
 
 export default getDiffTree;
